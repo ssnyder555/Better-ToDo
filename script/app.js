fd76125461a1714a660197e5b924024a8b17d618
ssnyder555 <ssnyder555@live.com>
2018-12-07 11:06:02 -0600

    jQuery is done!

--- script/app.js
+++ script/app.js
@@ -1,24 +1,28 @@
 
 // check off spacific todo by clicking
-$("li").click(function(){
+// really only listening to the li's inside of ul
+$('ul').on('click', 'li', function(){
 $(this).toggleClass('completed')
 });
 
 
 // click on x to delete todo
-$('span').click(function(event){
+// we are listening with the page for when on-"click" is hit
+// we only want code to run when a <span> is hit
+$('ul').on('click', 'span', function(event){
   $(this).parent().fadeOut(500, function() {
     $(this).remove();
   });
   event.stopPropagation();
 });
 
-//
+// creating new todo's
 $("input[type='text']").keypress(function(event){
   if(event.which === 13){
     //grabbing new todo text from input
   var toDoText = $(this).val()
+  $(this).val('')
   // create a new li and add to ul
-  $('ul').append('<li>' + toDoText + '</li>');
+  $('ul').append('<li><span>X </span>' + toDoText + '</li>');
   }
 });

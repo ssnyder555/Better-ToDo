de70dd0c62547a198f355783e61976bf46976b1a
ssnyder555 <ssnyder555@live.com>
2018-12-06 18:11:29 -0600

    line though added

--- script/app.js
+++ script/app.js
@@ -1,6 +1,16 @@
 
 // check off spacific todo by clicking
 $("li").click(function(){
-  $(this).css('color', 'red');
-  $(this).css('text-decoration', 'line-through');
+  // if li is gray we will turn it black
+  if($(this).css('color') === 'rgb(128, 128, 128)'){
+    $(this).css({
+      color: 'black',
+      textDecoration: 'none'
+    })
+  } else {
+    $(this).css({
+      color: 'gray',
+      textDecoration: 'line-through'
+    });
+  }
 });
